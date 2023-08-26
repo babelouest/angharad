20b5df81e2acf40ec4e97a85215ff353a1e0b12c
Nicolas Mora <github@babelouest.org>
2023-08-26 10:31:39 -0400

    Add component to get updated status for mpd service in map

---
+++ webapp-src/src/Angharad/MapMpdServiceButton.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import i18next from 'i18next';
+
+import messageDispatcher from '../lib/MessageDispatcher';
+import apiManager from '../lib/APIManager';
+
+class MapMpdServiceButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      element: props.element,
+      index: props.index,
+      style: props.style,
+      selectCb: props.selectCb,
+      adminMode: props.adminMode,
+      status: {state: "stop"}
+    }
+    
+    this.getPlayerStatus = this.getPlayerStatus.bind(this);
+    this.loopPlayerStatus = this.loopPlayerStatus.bind(this);
+    
+    this.loopPlayerStatus();
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return props;
+  }
+  
+  loopPlayerStatus() {
+    this.getPlayerStatus()
+    .then(() => {
+      let time = 300000;
+      if (this.state.status.state !== "stop") {
+        time = 10000;
+      }
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        this.loopPlayerStatus();
+      }, time);
+    });
+  }
+
+  getPlayerStatus() {
+    return apiManager.APIRequestCarleon("service-mpd/" + encodeURIComponent(this.state.element.name) + "/status")
+    .then(result => {
+      return this.setState({status: result});
+    })
+    .catch(err => {
+      messageDispatcher.sendMessage('Notification', {type: "danger", message: i18next.t("message.api-error")});
+    });
+  }
+  
+  render() {
+    let className = "map-img-element btn";
+    if (this.state.adminMode) {
+      className += " btn-danger";
+    } else {
+      if (this.state.status.state === "stop") {
+        className += " btn-secondary";
+      } else {
+        className += " btn-warning";
+      }
+    }
+    return (
+      <a href="#" className={className} title={this.state.element.name} style={this.state.style} onClick={(e) => this.state.selectCb(e, this.state.element, this.state.index)}>
+        <i className="fa fa-music" aria-hidden="true"></i>
+      </a>
+    );
+  }
+}
+
+export default MapMpdServiceButton;
